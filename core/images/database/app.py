f682ddb59c572b59974865fa1c2158cfd7288cfe
matluuk <85513088+matluuk@users.noreply.github.com>
2025-02-17 12:00:28 +0200

    add metrics + grafana dashboard fro database crud

--- core/images/database/app.py
+++ core/images/database/app.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from crud import router
 
-app = FastAPI()
+app = FastAPI(title="Database crud service")
 
 # Include the router
 app.include_router(router, prefix="/api/v1")
+
+# Add Prometheus instrumentation
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 async def root():
